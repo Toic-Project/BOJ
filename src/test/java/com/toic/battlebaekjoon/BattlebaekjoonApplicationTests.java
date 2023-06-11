59c2f41257d3338b14a8efe54492d9ed2dacdb1f
establers <establers@naver.com>
2023-06-11 21:28:55 +0900

    First Commit

---
+++ src/test/java/com/toic/battlebaekjoon/BattlebaekjoonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toic.battlebaekjoon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BattlebaekjoonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
